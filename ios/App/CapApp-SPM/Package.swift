754fbc1aac7233d3f32b028ebaa30739adc7756f
noid80-cmd <noid80@hanmail.net>
2026-09-05 14:49:31 +0900

    푸시 엔타이틀먼트와 SPM 설정을 git에 남긴다
    Push Notifications capability는 프로젝트에 붙어 있었는데 App.entitlements가
    빈 dict였고 CODE_SIGN_ENTITLEMENTS 연결도 없었다. Xcode를 여니 그제서야
    aps-environment를 채우고 연결했다 - 그동안 커밋된 적이 없어서 이 맥미니
    바깥에서는 설정이 존재하지도 않았다.
    
    플러그인 미등록과 합쳐서, 1.0.3은 푸시가 세 겹으로 막힌 채 나갔다.

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.2"),
+        .package(name: "CapacitorFirebaseMessaging", path: "symlinks/CapacitorFirebaseMessaging"),
         .package(name: "CapacitorPreferences", path: "../../../node_modules/@capacitor/preferences")
     ],
     targets: [
@@ -20,6 +21,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorFirebaseMessaging", package: "CapacitorFirebaseMessaging"),
                 .product(name: "CapacitorPreferences", package: "CapacitorPreferences")
             ]
         )
